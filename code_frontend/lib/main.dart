073e3d4c73916be40d181b2ed4c70dd3859fa808
natran10092004 <email_cua_ban@domain.com>
2025-11-27 15:26:13 +0700

    naaa

--- code_frontend/lib/main.dart
+++ code_frontend/lib/main.dart
@@ -24,6 +24,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.green,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
+      debugShowCheckedModeBanner: false,
       home: Consumer<AppState>(
         builder: (context, appState, child) {
           if (appState.currentUser == null) {
