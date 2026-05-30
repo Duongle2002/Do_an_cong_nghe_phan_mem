f55a3cc2ff0b060f5ec61dbd0227274550cdf7ba
huudu <duong100008@donga.edu.vn>
2026-05-30 08:43:15 +0700

    feat: add PresentationPage to display project presentation using an iframe - duong

--- smart-farm-dashboard/src/pages/PresentationPage.jsx
+++ smart-farm-dashboard/src/pages/PresentationPage.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import presentationHtml from '../../public/presentation.html?raw'
 
 export default function PresentationPage() {
   return (
     <div style={{ width: '100vw', height: '100vh', overflow: 'hidden', margin: 0, padding: 0 }}>
       <iframe
-        src="/presentation.html"
+        srcDoc={presentationHtml}
         style={{ width: '100%', height: '100%', border: 'none', display: 'block' }}
         title="Bài thuyết trình"
       />
